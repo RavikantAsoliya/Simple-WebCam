dd46e78347008751af57ecd1bd28c27db366a66a
Ravikant-Asoliya <103611650+Ravikant-Asoliya@users.noreply.github.com>
2022-06-15 21:46:27 +0530

    Version 1.0

---
+++ Simple WebCam/SimpleWebCam.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Simple_WebCam
+{
+    partial class SimpleWebCam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimpleWebCam));
+            this.displayPictureBox = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.devicesComboBox = new System.Windows.Forms.ComboBox();
+            this.rightClickMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.devicesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fullScreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.defaultToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.flipCameraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customSizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.roundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showInTaskbarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.displayPictureBox)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.rightClickMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // displayPictureBox
+            // 
+            this.displayPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.displayPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("displayPictureBox.Image")));
+            this.displayPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.displayPictureBox.Name = "displayPictureBox";
+            this.displayPictureBox.Size = new System.Drawing.Size(420, 236);
+            this.displayPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.displayPictureBox.TabIndex = 2;
+            this.displayPictureBox.TabStop = false;
+            this.displayPictureBox.DoubleClick += new System.EventHandler(this.DisplayPictureBox_DoubleClick);
+            this.displayPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PanelAsTitleBar_MouseDown);
+            this.displayPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PanelAsTitleBar_MouseMove);
+            this.displayPictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PanelAsTitleBar_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.displayPictureBox);
+            this.panel2.Controls.Add(this.devicesComboBox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(420, 236);
+            this.panel2.TabIndex = 7;
+            // 
+            // devicesComboBox
+            // 
+            this.devicesComboBox.Location = new System.Drawing.Point(37, 125);
+            this.devicesComboBox.Name = "devicesComboBox";
+            this.devicesComboBox.Size = new System.Drawing.Size(121, 21);
+            this.devicesComboBox.TabIndex = 0;
+            // 
+            // rightClickMenu
+            // 
+            this.rightClickMenu.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightClickMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.rightClickMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.devicesToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.pinToolStripMenuItem,
+            this.fullScreenToolStripMenuItem,
+            this.defaultToolStripMenuItem,
+            this.flipCameraToolStripMenuItem,
+            this.customSizeToolStripMenuItem,
+            this.roundToolStripMenuItem,
+            this.showInTaskbarToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.rightClickMenu.Name = "contextMenuStrip1";
+            this.rightClickMenu.Size = new System.Drawing.Size(159, 224);
+            // 
+            // devicesToolStripMenuItem
+            // 
+            this.devicesToolStripMenuItem.Name = "devicesToolStripMenuItem";
+            this.devicesToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.devicesToolStripMenuItem.Text = "Devices";
+            this.devicesToolStripMenuItem.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.DevicesToolStripMenuItem_DropDownItemClicked);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Enabled = false;
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.StopToolStripMenuItem_Click);
+            // 
+            // pinToolStripMenuItem
+            // 
+            this.pinToolStripMenuItem.Checked = true;
+            this.pinToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.pinToolStripMenuItem.Name = "pinToolStripMenuItem";
+            this.pinToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.pinToolStripMenuItem.Text = "Pin";
+            this.pinToolStripMenuItem.Click += new System.EventHandler(this.PinToolStripMenuItem_Click);
+            // 
+            // fullScreenToolStripMenuItem
+            // 
+            this.fullScreenToolStripMenuItem.CheckOnClick = true;
+            this.fullScreenToolStripMenuItem.Name = "fullScreenToolStripMenuItem";
+            this.fullScreenToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.fullScreenToolStripMenuItem.Text = "Full Screen";
+            this.fullScreenToolStripMenuItem.Click += new System.EventHandler(this.FullScreenToolStripMenuItem_Click);
+            // 
+            // defaultToolStripMenuItem
+            // 
+            this.defaultToolStripMenuItem.Name = "defaultToolStripMenuItem";
+            this.defaultToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.defaultToolStripMenuItem.Text = "Default";
+            this.defaultToolStripMenuItem.Visible = false;
+            this.defaultToolStripMenuItem.Click += new System.EventHandler(this.DefaultToolStripMenuItem_Click);
+            // 
+            // flipCameraToolStripMenuItem
+            // 
+            this.flipCameraToolStripMenuItem.Name = "flipCameraToolStripMenuItem";
+            this.flipCameraToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.flipCameraToolStripMenuItem.Text = "Flip Camera";
+            this.flipCameraToolStripMenuItem.Click += new System.EventHandler(this.FlipCameraToolStripMenuItem_Click);
+            // 
+            // customSizeToolStripMenuItem
+            // 
+            this.customSizeToolStripMenuItem.Name = "customSizeToolStripMenuItem";
+            this.customSizeToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.customSizeToolStripMenuItem.Text = "Custom Size";
+            this.customSizeToolStripMenuItem.Visible = false;
+            this.customSizeToolStripMenuItem.Click += new System.EventHandler(this.CustomSizeToolStripMenuItem_Click);
+            // 
+            // roundToolStripMenuItem
+            // 
+            this.roundToolStripMenuItem.CheckOnClick = true;
+            this.roundToolStripMenuItem.Name = "roundToolStripMenuItem";
+            this.roundToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.roundToolStripMenuItem.Text = "Round Window";
+            this.roundToolStripMenuItem.Click += new System.EventHandler(this.RoundToolStripMenuItem_Click);
+            // 
+            // showInTaskbarToolStripMenuItem
+            // 
+            this.showInTaskbarToolStripMenuItem.CheckOnClick = true;
+            this.showInTaskbarToolStripMenuItem.Name = "showInTaskbarToolStripMenuItem";
+            this.showInTaskbarToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.showInTaskbarToolStripMenuItem.Text = "Show In Taskbar";
+            this.showInTaskbarToolStripMenuItem.Click += new System.EventHandler(this.ShowInTaskbarToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // SimpleWebCam
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(420, 236);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(200, 113);
+            this.Name = "SimpleWebCam";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple WebCam";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.displayPictureBox)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.rightClickMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox displayPictureBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ContextMenuStrip rightClickMenu;
+        private System.Windows.Forms.ToolStripMenuItem devicesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fullScreenToolStripMenuItem;
+        private System.Windows.Forms.ComboBox devicesComboBox;
+        private System.Windows.Forms.ToolStripMenuItem pinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem flipCameraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem defaultToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customSizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem roundToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showInTaskbarToolStripMenuItem;
+    }
+}
+
---
+++ Simple WebCam/SimpleWebCam.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Simple_WebCam.Properties;
+using System.Drawing.Drawing2D;
+
+namespace Simple_WebCam
+{
+    public partial class SimpleWebCam : Form
+    {
+        #region SomeUseFullVariables
+        private FilterInfoCollection devicesList; // To Get List of Availabel Camera Devices
+        private VideoCaptureDevice currentDevice = null; // Get One Device from List of Available Devices
+        private bool mouseDrag; // To check movement of App
+        private Point offset; // Get Current Location of Window
+        private bool isFlip; // To check video frame flipped or not
+        private readonly ComponentResourceManager resources = new ComponentResourceManager(typeof(SimpleWebCam)); // to use resources
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        public static extern IntPtr CreateRoundRectRgn(int nLeftRect,
+                                               int nRightRect,
+                                               int nTopRect,
+                                               int nBottomRect,
+                                               int nWidthEllipse,
+                                               int nHeightRectEllipse);
+        #endregion
+
+        public SimpleWebCam()
+        {
+            InitializeComponent();
+            ContextMenuStrip = rightClickMenu;
+            isFlip = true; //Default value is true, You will see your image on screen as Mirror.
+            devicesComboBox.Visible = false;// Hide Combobox
+            FindDevices(); // Find All Camera devices and add in Devices Section of ContextMenuStrip Dynamically
+        }
+
+
+        #region Save Default State And Location
+        private void Form1_Load(object sender, EventArgs e) // It will capture the previous location of the app. and your app will start at that location.
+        {
+            WindowState = Settings.Default.F1State;
+            if (WindowState == FormWindowState.Maximized)
+            {
+                fullScreenToolStripMenuItem.Checked = true;
+            }
+
+            Location = Settings.Default.F1Location;
+        } 
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) // It will capture the current location of your app and will save it in the config file.
+        {
+            StopToolStripMenuItem_Click(sender, e);
+
+            Settings.Default.F1State = WindowState;
+            if (WindowState == FormWindowState.Normal)
+            {
+                Settings.Default.F1Location = Location;
+            }
+            else
+            {
+                Settings.Default.F1Location = RestoreBounds.Location;
+            }
+
+            Settings.Default.Save();
+        }
+
+        #endregion
+
+
+        #region Function of ContextMenu ( Right Click Menu )
+
+        /// <summary>
+        /// This will find all camera devices that are connected to your computer via WiFi, cable, or that are integrated.
+        /// </summary>
+        private void FindDevices()
+        {
+            devicesComboBox.Items.Clear();
+
+            devicesList = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (devicesList.Count > 0)
+            {
+                devicesToolStripMenuItem.DropDownItems.Clear();
+                foreach (FilterInfo device in devicesList)
+                {
+                    devicesComboBox.Items.Add(device.Name);
+
+                    devicesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] {
+                        new ToolStripMenuItem
+                        {
+                            Name = device.Name + "ToolStripMenuItem",
+                            Text = device.Name,
+                            Size = new Size(270, 34),
+                        }
+                    });
+                }
+            }
+        }
+
+        /// <summary>
+        /// It will show you all the available devices and then if you click on one of them, your camera will be triggered.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DevicesToolStripMenuItem_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            void StartCamera()
+            {
+                if (currentDevice != null)
+                {
+                    currentDevice = new VideoCaptureDevice(devicesList[devicesComboBox.SelectedIndex].MonikerString);
+                    currentDevice.NewFrame += CurrentDevice_NewFrame;
+                    currentDevice.Start();
+                }
+            }
+            string menuname = e.ClickedItem.Text;
+            devicesComboBox.SelectedIndex = devicesComboBox.Items.IndexOf(menuname);
+            currentDevice = new VideoCaptureDevice(devicesList[devicesComboBox.SelectedIndex].MonikerString);
+            devicesToolStripMenuItem.Enabled = false;
+            StartCamera(); // Start Camera Automatically
+            stopToolStripMenuItem.Enabled = true;
+        }
+
+        private void CurrentDevice_NewFrame(object sender, NewFrameEventArgs eventArgs) // Get new Images
+        {
+            Bitmap frame = (Bitmap)eventArgs.Frame.Clone();
+            if (isFlip)
+            {
+                frame.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            }
+            displayPictureBox.Image = frame;
+
+        }
+
+        private void StopToolStripMenuItem_Click(object sender, EventArgs e) // Stop Camera
+        {
+            if (currentDevice != null)
+            {
+                currentDevice.Stop();
+                currentDevice.NewFrame -= CurrentDevice_NewFrame;
+            }
+            displayPictureBox.Image = (Image)resources.GetObject("displayPictureBox.Image");
+            devicesToolStripMenuItem.Enabled = true;
+            stopToolStripMenuItem.Enabled = false;
+            foreach (ToolStripMenuItem item in devicesToolStripMenuItem.DropDownItems)
+            {
+                //Console.WriteLine(item.Name);
+                item.Checked = false;
+            }
+        }
+
+        private void PinToolStripMenuItem_Click(object sender, EventArgs e) // For Make Top of the Most Window
+        {
+            if (pinToolStripMenuItem.Checked)
+            {
+                TopMost = false;
+                pinToolStripMenuItem.Checked = false;
+            }
+            else
+            {
+                pinToolStripMenuItem.Checked = true;
+                TopMost = true;
+            }
+        }
+
+        private void FullScreenToolStripMenuItem_Click(object sender, EventArgs e) // For FullScreen
+        {
+            if (fullScreenToolStripMenuItem.Checked)
+            {
+                roundToolStripMenuItem.Checked = false;
+                roundToolStripMenuItem.Enabled = false;
+                WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                roundToolStripMenuItem.Checked = false;
+                roundToolStripMenuItem.Enabled = true;
+                WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void DefaultToolStripMenuItem_Click(object sender, EventArgs e) // this function is not working.
+        {
+            Console.WriteLine("This Function is not Working.");
+        }
+
+        private void FlipCameraToolStripMenuItem_Click(object sender, EventArgs e) // To Mirror Camera Image
+        {
+            if (flipCameraToolStripMenuItem.Checked)
+            {
+                isFlip = true;
+                flipCameraToolStripMenuItem.Checked = false;
+            }
+            else
+            {
+                isFlip = false;
+                flipCameraToolStripMenuItem.Checked = true;
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e) // For Exit And Close App 
+        {
+            StopToolStripMenuItem_Click(sender, e);
+            Close();
+        }
+
+        private void CustomSizeToolStripMenuItem_Click(object sender, EventArgs e) // This Function is not working.
+        {
+            Console.WriteLine("This Function is not Working.");
+        }
+
+        private void RoundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (roundToolStripMenuItem.Checked)
+            {
+                GraphicsPath path = new GraphicsPath();
+                path.AddEllipse(90, 0, displayPictureBox.Height, displayPictureBox.Height);
+                ActiveForm.Region = new Region(path);
+                displayPictureBox.SizeMode = PictureBoxSizeMode.StretchImage; //CenterImage
+                fullScreenToolStripMenuItem.Checked = fullScreenToolStripMenuItem.Enabled = false;
+            }
+            else
+            {
+                ActiveForm.Size = new Size(420, 236);
+                Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height, 0, 0));
+                fullScreenToolStripMenuItem.Checked = false;
+                fullScreenToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void ShowInTaskbarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (showInTaskbarToolStripMenuItem.Checked == true)
+                ShowInTaskbar = true;
+            else
+                ShowInTaskbar = false;
+        }
+
+        #endregion
+
+
+        #region  Drag Application
+        private void PanelAsTitleBar_MouseDown(object sender, MouseEventArgs e) // Get App Current Position
+        {
+            offset.X = e.X;
+            offset.Y = e.Y;
+            mouseDrag = true;
+        }
+
+        private void PanelAsTitleBar_MouseUp(object sender, MouseEventArgs e) // Change mouseDrag = false
+        {
+            mouseDrag = false;
+        }
+
+        private void PanelAsTitleBar_MouseMove(object sender, MouseEventArgs e) // To Move window in specific location
+        {
+            if (mouseDrag == true)
+            {
+                Point currentScreenPosition = PointToScreen(e.Location);
+                Location = new Point(currentScreenPosition.X - offset.X, currentScreenPosition.Y - offset.Y);
+            }
+        }
+        #endregion
+
+
+        private void DisplayPictureBox_DoubleClick(object sender, EventArgs e)
+        {
+            if (roundToolStripMenuItem.Checked)
+            {
+
+            }
+            else
+            {
+                if (fullScreenToolStripMenuItem.Checked)
+                {
+                    ActiveForm.WindowState = FormWindowState.Normal;
+                    fullScreenToolStripMenuItem.Checked = false;
+                    roundToolStripMenuItem.Checked = false;
+                    roundToolStripMenuItem.Enabled = true;
+                }
+                else
+                {
+                    ActiveForm.WindowState = FormWindowState.Maximized;
+                    fullScreenToolStripMenuItem.Checked = true;
+                    roundToolStripMenuItem.Checked = false;
+                    roundToolStripMenuItem.Enabled = false;
+                }
+            }
+        }
+
+    }
+}
